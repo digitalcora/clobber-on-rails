2c0b582bba6092ed35485c1acad50d694e96798c
Alex Grant <alex@grantovich.net>
2011-04-18 16:44:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clobber::Application.config.secret_token = '13e4aece638d13342b261564346195223dd6abd5a927587743f33078b396db352c974555b5ddcea2ee5f3be6a9f94c6df7ac7c719e52e542d9c4376719dab5c3'
