47124fb54fa7e44f09e5f41e975be0bb1f320094
Alex Grant <alex@grantovich.net>
2011-04-20 19:15:38 -0400

    Unbroke user code... didn't copy/paste some important things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = 'User info updated.'
-      redirect_to @user
+      redirect_to root_path
     else
       @title = 'Edit user info'
       render 'edit'
