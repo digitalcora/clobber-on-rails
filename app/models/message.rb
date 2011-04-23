4394b0eecafa39f4963d06146722c7d27b8e65cb
Alex Grant <alex@grantovich.net>
2011-04-23 16:46:10 -0400

    Added message validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,15 @@ class Message < ActiveRecord::Base
   
   belongs_to :user
   belongs_to :game
+  
+  validates :user_id, :presence => true
+  validates :content, :presence => true,
+                      :length => { :maximum => 250 }
+  def validate
+    unless game_id.nil? or Game.find_by_id(game_id)
+      errors.add(:game_id, 'must be nil or a valid game')
+    end
+  end
+  
+  
 end
