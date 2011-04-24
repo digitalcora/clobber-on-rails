9b176decd324324fd5270d7b0221fc215963116c
Alex Grant <alex@grantovich.net>
2011-04-23 20:48:12 -0400

    Better styles and some cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     if signed_in?
+      @title = 'Game Lobby'
       @messages = Message.where(:game_id => nil, :created_at => (Time.now - 1.day)..Time.now)
     end
   end
