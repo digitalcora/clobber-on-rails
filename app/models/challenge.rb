3760bcaa378894e4cf3ef3cdafafe1a45efcd677
Alex Grant <alex@grantovich.net>
2011-04-28 17:45:25 -0400

    Convert validations to use validates_existence plugin

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,15 +4,8 @@ class Challenge < ActiveRecord::Base
   belongs_to :user
   belongs_to :target, :class_name => 'User'
   
-  validates :user, :presence => { :unique => true }
-  validates :target, :presence => { :unique => true }
-  
-  def validate
-    if not User.find_by_id(user_id)
-      errors.add(:user_id, 'must be a valid user')
-    end
-    if not User.find_by_id(target_id)
-      errors.add(:target_id, 'must be a valid user')
-    end
-  end
+  validates :user_id, :uniqueness => true,
+                      :existence => { :both => false }
+  validates :target_id, :uniqueness => true,
+                        :existence => { :both => false }
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,8 @@ class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
   
-  validates :user_id, :presence => true
+  validates :user_id, :existence => { :allow_nil => true, :both => false }
+  validates :game_id, :existence => { :allow_nil => true, :both => false }
   validates :content, :presence => true,
                       :length => { :maximum => 250 }
-  def validate
-    unless game_id.nil? or Game.find_by_id(game_id)
-      errors.add(:game_id, 'must be nil or a valid game')
-    end
-  end
 end
