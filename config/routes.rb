309cf970b5b34ddf99ab62d7345f8ed1ca5fbd50
Alex Grant <alex@grantovich.net>
2011-05-04 18:15:29 -0400

    Remove redundant holdover from tutorial app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Clobber::Application.routes.draw do
-  get 'sessions/new'
-  
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
   resources :messages, :only => [:new, :create]
