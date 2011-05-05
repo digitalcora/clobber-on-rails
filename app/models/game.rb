3ad6af9eafcb7e3481b58ad2df684cc45b79e040
Alex Grant <alex@grantovich.net>
2011-05-05 17:46:44 -0400

    Set games and players active by default

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,12 @@
 class Game < ActiveRecord::Base
   attr_accessible # No accessible attributes
+  after_initialize :set_active
   
   has_many :messages
   has_many :players
   has_many :pieces, :through => :players
+  
+  def set_active
+    self.active = true
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,12 @@
 class Player < ActiveRecord::Base
   attr_accessible # No accessible attributes
+  after_initialize :set_active
   
   belongs_to :user
   belongs_to :game
   has_many :pieces
+  
+  def set_active
+    self.active = true
+  end
 end
