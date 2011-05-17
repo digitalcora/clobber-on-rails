ed0b7c361dc1466ca4942d026c3daedbdfab9b41
Alex Grant <alex@grantovich.net>
2011-05-17 16:24:25 -0400

    Implement game creation from challenge acceptance

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,13 +2,37 @@ class GamesController < ApplicationController
   before_filter :authenticate
   before_filter :find_records, :except => :create
   before_filter :setup_show, :only => [:show, :edit]
+  before_filter :find_challenge, :only => :create
 
   def show
     # Nothing special
   end
   
   def create
+    @game = Game.create!
+    [@challenge.user, @challenge.target_user].each_with_index do |user, i|
+      player = user.players.build
+      player.game = @game
+      player.turn_order = i + 1
+      player.save!
+    end
+    @challenge.destroy
     
+    players = @game.players.each
+    player = players.next
+    @game.height.times do |y|
+      @game.width.times do |x|
+        player.pieces.create!(:x => x, :y => y)
+        begin
+          player = players.next
+        rescue StopIteration
+          players.rewind
+          retry
+        end
+      end
+    end
+    
+    redirect_to @game
   end
   
   def edit
@@ -39,5 +63,13 @@ class GamesController < ApplicationController
           map{ |p| p.user.username }.join(', ')
       @message = Message.new
       @messages = @game.messages
+    end
+    
+    def find_challenge
+      @challenge = Challenge.find_by_id(params[:challenge_id])
+      if @challenge.nil?
+        flash[:notice] = 'The challenge you were attempting to accept has been canceled.'
+        redirect_to :back
+      end
     end
 end
