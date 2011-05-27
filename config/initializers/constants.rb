95d3f9b2aba0aeee0e153c160b3229180a93c9fd
Alex Grant <alex@grantovich.net>
2011-05-27 15:25:12 -0400

    Un-hardcode player colors

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,3 +4,11 @@ TILE_SIZE = 100.0
 
 # Time in milliseconds between ajax updates
 UPDATE_DELAY = 750
+
+# Player/piece colors and color names
+PLAYER_COLORS = [
+  [ 'red',  'red',  '#a11' ],
+  [ 'blue', 'blue', '#119' ]
+].map do |colors|
+  { :name => colors[0], :primary => colors[1], :secondary => colors[2] }
+end
