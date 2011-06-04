be1b7f821eb0e6d70710b763e790610372677b59
Alex Grant <alex@grantovich.net>
2011-06-04 13:29:47 -0400

    Put header js where it's supposed to be

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,32 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  // Try to focus any inputs that need focusing
+  $('#session_username').focus();
+  $('#user_username').focus();
+  $('#message_content').focus();
+  
+  // Make message form clear on submission
+  $('#new_message').live('ajax:beforeSend', function() {
+    $('#message_content').val('').focus();
+  });
+  
+  // Get rid of any flashes hanging around
+  $('.flash').delay(4000).slideUp('', function(){$(this).remove();});
+  
+  // Add inline help handler
+  $('#help_pane').hide();
+  $('.help_link').click(function(e) {
+    $('#help_pane').slideToggle();
+    e.preventDefault();
+  });
+});
+
+// Handle ajax responses with a flash attached
+$(document).ajaxComplete(function(e, request, opts) {
+  if (request.getResponseHeader('X-Message') != null) {
+    var flash = $('<div class="flash ' +
+      request.getResponseHeader('X-Message-Type') + '">' +
+      request.getResponseHeader('X-Message') + '</div>');
+    $('#flashes').append(flash);
+    flash.hide().slideDown().delay(4000).slideUp('', function(){$(this).remove();});
+  }
+});
